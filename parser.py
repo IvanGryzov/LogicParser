4d661804f52cbd7dadcf2cc521ebc6f4e4489475
Ivan <%-1966>
2017-04-17 15:22:30 +0600

    Первая заготовка 1

--- parser.py
+++ parser.py
@@ -1,5 +1,20 @@
+fr=[]
+pos=0
 
+def OrOp():
+    pass
 
+def NotOp():
+    global pos,fr
+    if fr[pos] in "ABCDEFGH":
+        return False
+    elif fr[pos]=='(':
+        pos+=1
+        return OrOp()
+        pos+=1
+    elif fr[pos]=='not':
+        pos+=1
+        return not NotOp()
 
 def Parser(s):
     """
@@ -7,7 +22,11 @@ def Parser(s):
     :param s: Строка с формулой 
     :return: значение
     """
+    global fr
     fr = s.split()
     print(fr)
+    pos=0
+    print(NotOp())
 
-Parser('A and B or not C')
+Parser('not C')
+print(fr)
